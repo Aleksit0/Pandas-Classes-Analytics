26d3fc59017796f70cfb877db6e31f5d6a9b2971
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2020-12-26 11:57:03 +0100

    Basic table

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+# pravimo podatke, unosimo ocjene
+raw_data = np.random.uniform(6, size = (5, 3))
+# zaokruzujemo random brojeve na 2 decimale
+obradjena_data = np.round(raw_data, decimals = 2, out = None)
+
+print('\n')
+
+# pravimo indexe, ucenike
+ucenici = ['1', '2', '3', '4', '5']
+
+# pravimo kolone, odjeljenja
+odjeljenja = ['Class 1', 'Class 2', 'Class 3']
+
+# pravimo table
+df = pd.DataFrame(obradjena_data, index = ucenici, columns = odjeljenja)
+print(df)
+
+
